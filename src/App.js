c9685629e7e4e52b6a2e0aa2fce8cdd1ac6788cf
Shaukat Sohail <65353819+Shaukat456@users.noreply.github.com>
2022-11-17 19:30:57 +0500

    navbar

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import './App.css';
 import Cards from './components/Cards';
 import WelcomeBack from './components/WelcomeBack';
 import Slider from './components/Slider';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-    <div className='lg:flex w-full  '>
-
-  <Cards />
-  <Cards />
-  <Cards />
-    </div>
+    {/* <div className='lg:flex w-full  '> */}
+<Navbar/>
+  <Cards  />
+  {/* <Cards /> */} 
+    {/* </div> */}
   <WelcomeBack/>
-  {/* <Slider/> */}
+  
+  <Slider/>
+
   
 
     </>
---
+++ src/components/Navbar.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+ const Navbar = () => {
+  return (
+    <>
+    <div className="text-gray-600 body-font backdrop-blur fixed w-full  ">
+  <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+ <button>
+
+      {/* <span className="ml-3 text-xl"> */}
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+</svg>
+
+      {/* </span>      */}
+   
+ </button>
+
+    <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start space-x-5">
+      <a className="ml-3 text-gray-500">
+        {/* <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+        </svg> */}
+        <figure>
+            <img src="https://minimals.cc/assets/icons/flags/ic_flag_en.svg" alt="" />
+        </figure>
+      </a>
+        {/* <figure >
+            <img height={20} width={30} src={require("../avatar.png")} alt="" />
+        </figure> */}
+      <a className="ml-3 text-gray-500">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+</svg>
+
+      </a>
+      <a className="ml-3 text-gray-500">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5" />
+</svg>
+
+      </a>
+    </span>
+  </div>
+</div>
+    
+    </>
+  )
+}
+
+
+export default Navbar;
