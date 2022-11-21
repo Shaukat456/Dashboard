6308189151b7bb88df1fb82e1978190e3255b2d5
Shaukat Sohail <65353819+Shaukat456@users.noreply.github.com>
2022-11-21 15:32:56 +0500

    navbar wb

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,8 @@ import React from 'react'
  const Navbar = () => {
   return (
     <>
-    <div className="text-gray-600 body-font backdrop-blur fixed w-full  ">
-  <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+    <div className="text-gray-600 body-font backdrop-blur fixed w-full md:space-x-5 space-x-52 ">
+  <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row sm:justify-evenly justify-center   ">
  <button>
 
       {/* <span className="ml-3 text-xl"> */}
@@ -29,16 +29,22 @@ import React from 'react'
             <img height={20} width={30} src={require("../avatar.png")} alt="" />
         </figure> */}
       <a className="ml-3 text-gray-500">
+     <button>
+
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
 </svg>
+     </button>
 
       </a>
+
       <a className="ml-3 text-gray-500">
+      <button>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5" />
 </svg>
 
+      </button>
       </a>
     </span>
   </div>
--- src/components/WelcomeBack.js
+++ src/components/WelcomeBack.js
@@ -4,23 +4,23 @@ const WelcomeBack = () => {
   return (
     <div className='rounded-3xl'>
       <section class="text-gray-600 body-font bg-[#ccf2e1]  ">
-  <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-    <div class="lg:flex-grow md:w-1/2   lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left lg:flex  mb-16 md:mb-0 items-center text-center ">
-      <h1 class="title-font lg:text-7xl    sm:text-4xl  text-2xl font-bold text-gray-600 font-CardH md:text-4xl">Welcome Back! 
+  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div className="lg:flex-grow md:w-1/2   lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left lg:flex  mb-16 md:mb-0 items-center text-center ">
+      <h1 className="title-font lg:text-7xl    sm:text-4xl  text-2xl font-bold text-gray-600 font-CardH md:text-4xl">Welcome Back! 
         {/* <br class="hidden lg:inline-block"readymade gluten   /> */}
       </h1>
-      <h1 class=" font-CardH   title-font   mb-4  text-gray-600  text-2xl lg:text-7xl  md:text-4xl   sm:text-4xl font-bold">Minimal UI
+      <h1 className=" font-CardH   title-font   mb-4  text-gray-600  text-2xl lg:text-7xl  md:text-4xl   sm:text-4xl font-bold">Minimal UI
         {/* <br class="hidden lg:inline-block"readymade gluten   /> */}
       </h1>
-      <p class="mb-8 leading-relaxed text-gray-500 my-5 lg:text-3xl">If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything.</p>
-      <div class="flex justify-center">
-        <button class="inline-flex text-white bg-base border-0 py-2 px-6 focus:outline-none hover:bg-green-800 rounded-xl md:text-lg  font-bold  ">Go Now</button>
+      <p className="mb-8 leading-relaxed text-gray-500 my-5 lg:text-3xl">If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything.</p>
+      <div className="flex justify-center">
+        <button className="inline-flex text-white bg-base border-0 py-2 px-6 focus:outline-none hover:bg-green-800 rounded-xl md:text-lg  font-bold  ">Go Now</button>
         
       </div>
     </div>
-    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 flex ">
-        <svg></svg>
-      <img class="object-cover object-center rounded" alt="hero" src="https://minimals.cc/assets/illustrations/characters/character_3.png"/>
+    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 flex ">
+        {/* <svg></svg> */}
+      <img className="object-cover object-center rounded" alt="hero" src="https://minimals.cc/assets/illustrations/characters/character_3.png"/>
     </div>
   </div>
 </section>
