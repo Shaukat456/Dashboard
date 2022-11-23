f0d2ce1b23177e36b535c89aaf9e952b0f60ca48
Shaukat Sohail <65353819+Shaukat456@users.noreply.github.com>
2022-11-23 19:53:07 +0500

    line graph fixed

--- src/components/LineChartCard.js
+++ src/components/LineChartCard.js
@@ -4,8 +4,10 @@ import { LineChart } from './LineChart'
 export const LineChartCard = () => {
   return (
     <div className='rounded-2xl flex flex-col  w-3/5 p-10 bg-gray-100   '>
-            <div className="flex mx-5  space-y-6">
-            <h1>Area installed</h1>
+            <div className="flex mx-5  flex-col">
+            <h1 className='font-bold font-CardH'>Area installed</h1>
+
+            <h3 className='text-gray-500 text-start'>(+43%) than last year</h3>
 
             </div>
         <LineChart/>
