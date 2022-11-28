051435c8509da33a79375b5ee1a1471e3d548418
Shaukat Sohail <65353819+Shaukat456@users.noreply.github.com>
2022-11-28 15:50:40 +0500

    countr

--- src/components/TopC.js
+++ src/components/TopC.js
@@ -39,14 +39,14 @@ import Avatar from "../avatar.png";
   return (
     <>
       <div className="rounded-2xl text-gray-700    w-[550px]   p-4 pr-5   shadow-lg">
-        <h1 className="p-3 font-CardH text-xl font-semibold">Top Installed Countries</h1>
+        <h1 className="p-2 font-CardH text-xl font-semibold">Top Installed Countries</h1>
         {Authors.map(({ name, Likes, TrophyColor }, index) => {
           return (
             <>
-             <div className="grid  lg:grid-cols-4  p-3  space-x-5  ">
-              <div className="flex md:col-span-1 space-x-1  w-full items-center justify-center ">
+             <div className="grid  lg:grid-cols-4  p-2  space-x-5  ">
+              <div className="flex md:col-span-1 space-x-1  w-full items-center jstify-center ">
                 {/* <figure> */}
-                  <img className="w-full" src="https://minimals.cc/assets/icons/flags/ic_flag_de.svg" alt="" />
+                  <img className="" src="https://minimals.cc/assets/icons/flags/ic_flag_de.svg" alt="" />
                 {/* </figure> */}
                 <p className="">{name}</p>
               </div>
